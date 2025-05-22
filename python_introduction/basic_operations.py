6622db88482c255ef4d6f1599e6a9a4ae5412bc2
Martin Mulang <mulang.cmu@gmail.com>
2025-05-22 19:07:39 +0300

    Still resolving remote origin

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,22 @@
+"""
+A Python script that performs basic arithmetic operations with two predefined numbers.
+The script should do the following:
+    1. Assigns specific values to two variables, number1 and number2.
+    2. Performs addition, subtraction, and multiplication on these numbers.
+    3. Prints the results of these operations in a human-readable format.
+"""
+
+number1 = 10
+number2 = 5
+
+#Declaring variables
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+if __name__ == "__main__":
+    print(f"Addition of {number1} and {number2} is {sum}")
+    print(f"Subtraction of {number1} and {number2} is {difference}")
+    print(f"Multiplication of {number1} and {number2} is {product}")
+
+
