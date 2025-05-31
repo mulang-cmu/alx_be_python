9e3db0fc812aa33d91890faec0f1fe034f950b71
Martin Mulang <mulang.cmu@gmail.com>
2025-05-31 13:27:55 +0300

    Change match case in weather.py to if, else, elif to match checker requirements.

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -18,17 +18,15 @@ Task Description:
 # A function (Using match cases)that recommends weather clothing based on user input
 def recommend_wather_clothig():
     weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
-    match weather:
-        case "sunny":
-            recommend = "Wear a t-shirt and sunglasses."
-        case "rainy":
-            recommend = "Don't forget your umbrella and a raincoat."
-        case "cold":
-            recommend = "Make sure to wear a warm coat and a scarf."
-
-        case __:
-            print("Sorry, I don't have recommendations for this weather.")
-            return
+    if weather == "sunny":
+        recommend = "Wear a t-shirt and sunglasses."
+    elif weather == "rainy":
+        recommend = "Don't forget your umbrella and a raincoat."
+    elif weather == "cold":
+        recommend = "Make sure to wear a warm coat and a scarf."
+    else:
+        print("Sorry, I don't have recommendations for this weather.")
+        return
 
     return(recommend)
 
