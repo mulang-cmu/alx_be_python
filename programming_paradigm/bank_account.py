24ca75465f2c828b338b8bddf9715cf79b0bf768
Martin Mulang <mulang.cmu@gmail.com>
2025-06-14 14:06:16 +0300

    Add the dollar sighn to indicate currency in the balance amount

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -39,4 +39,4 @@ class BankAccount:
             return False        
 
     def display_balance(self):
-        print(f"Current Balance: {self.account_balance:.2f}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
